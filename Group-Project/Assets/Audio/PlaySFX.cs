4747745a3ea76945702bb7be0e637a33864e324f
ASUMSTAD <asumstad@utexas.edu>
2020-11-02 19:09:47 -0600

    Material Changes

---
+++ Group-Project/Assets/Audio/PlaySFX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour
+{
+    public bool enter;
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        enter = false;
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && enter == false)
+        {
+            enter = true;
+            // Play the sound only on the trigger
+            source.Play();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        source.Pause();
+    }
+}
