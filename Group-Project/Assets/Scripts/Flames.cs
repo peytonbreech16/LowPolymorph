db276f046284a4398b958520b770611ded8efded
Peyton Breech <peyton.breech16@gmail.com>
2020-11-06 11:28:06 -0600

    Flame spell working

---
+++ Group-Project/Assets/Scripts/Flames.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flames : MonoBehaviour
+{
+    void Start() 
+    {
+        
+    }
+    void Update() 
+    {
+        
+    }
+    //Collision for the flames spell to register on enemies.
+    private void OnParticleCollision(GameObject other) 
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other);
+        }
+    }
+}
