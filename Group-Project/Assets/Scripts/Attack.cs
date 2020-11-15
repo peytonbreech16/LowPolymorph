c44249e9ae05c0dd041fffb1c483cd2d6b711924
Peyton Breech <peyton.breech16@gmail.com>
2020-11-14 18:54:21 -0600

    Working Shells

--- Group-Project/Assets/Scripts/Attack.cs
+++ Group-Project/Assets/Scripts/Attack.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Attack : StateMachineBehaviour
 {
+    public int damage = 10;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        GameObject player = GameObject.Find("Player");
-       player.GetComponent<Player>().health -= 10;
+       player.GetComponent<Player>().health -= damage;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
